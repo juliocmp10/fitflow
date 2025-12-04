2da977988d71cf76955f0ac5d35a890ab8e5abf3
juliocmp10 <julio_c.m.p@hotmail.com>
2025-12-04 17:07:53 -0300

    reorg 2

--- index.tsx
+++ index.tsx
@@ -1,15 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Este arquivo foi movido para a pasta /fitflow
+// Por favor, utilize fitflow/index.tsx
